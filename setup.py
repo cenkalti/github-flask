168775d3e5f266404d07e586b0a3c6660ad92aae
Cenk Alti <cenkalti@gmail.com>
2013-06-29 11:30:14 +0300

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='GitHub-Flask',
-    version='0.1.0',
+    version='0.2.0',
     url='http://github.com/cenkalti/github-flask',
     license='MIT',
     author=u'Cenk Altı',

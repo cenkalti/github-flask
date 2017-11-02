f3f1358440849cfaa68c91f3418c1c81d47632f5
Cenk Alti <cenkalti@gmail.com>
2017-11-02 14:16:19 +0300

    bump version

--- flask_github.py
+++ flask_github.py
@@ -17,7 +17,7 @@ from functools import wraps
 import requests
 from flask import redirect, request, json
 
-__version__ = '3.1.7'
+__version__ = '3.2.0'
 
 _logger = logging.getLogger(__name__)
 # Add NullHandler to prevent logging warnings on startup
